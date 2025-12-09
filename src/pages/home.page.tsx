67f78ff8c0d41cacf8f366cefb1c99af5dc10117
Nirren404 <linajana290778@gmail.com>
2025-12-09 12:50:29 +0100

    Poster-style cards with title + genre. did map so it shows but with genre and title but poster style card not yet styling will be later

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,5 +1,12 @@
 const HomePage = () => {
-  return <div>home</div>;
+  // Poster-style cards with title + genre.
+
+  return (
+    <div>
+      <h1>Welcome to Movie Night Planner</h1>
+      <p>Plan your perfect movie night with us</p>
+    </div>
+  );
 };
 
 export default HomePage;
--- src/pages/movie.page.tsx
+++ src/pages/movie.page.tsx
@@ -1,5 +1,19 @@
+import { movies } from "../data/movies";
+
 const MoviePage = () => {
-  return <div>Movie</div>;
+  return (
+    <div>
+      <h1>Movies </h1>
+
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h2>{movie.title}</h2>
+
+          <p>Genre: {movie.genre}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MoviePage;
