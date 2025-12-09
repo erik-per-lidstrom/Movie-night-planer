3a78b27bb3b31c29e84b56c283b41a17489fd0d4
Nirren404 <linajana290778@gmail.com>
2025-12-09 10:34:34 +0100

    initial commit wrote hardcoded list and type for movie list

---
+++ src/data/movies.ts
@@ -0,0 +1,24 @@
+import type { Movies } from "../types";
+
+export const movies: Movies = [
+  {
+    ageRate: "PG-13",
+    title: "Inception",
+    genre: "Science Fiction",
+  },
+  {
+    title: "Non Stop",
+    ageRate: "PG-13",
+    genre: "Crime",
+  },
+  {
+    title: "Pulp Fiction",
+    ageRate: "PG-18",
+    genre: "Crime",
+  },
+  {
+    title: "The Five nights at Freddy's Movie",
+    ageRate: "PG-13",
+    genre: "Horror",
+  },
+];
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type Movie = {
+  ageRate: string;
+  title: string;
+  genre: string;
+};
